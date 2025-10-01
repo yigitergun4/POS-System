6fce57a499c648e8f649b3aecd99e0d08781f747
yigitergun <yigitergunn4@gmail.com>
2025-10-01 20:23:38 +0300

    sda

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
+  base: "/POS-System/",
   build: {
     chunkSizeWarningLimit: 1600,
   },
