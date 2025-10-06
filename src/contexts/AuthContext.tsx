f2c2d8437c961de15cb88737a575d63535ec338c
yigitergun <yigitergunn4@gmail.com>
2025-10-06 11:29:08 +0300

    changes for github

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -15,6 +15,7 @@ type User = {
 
 type AuthContextType = {
   user: User | null;
+  loading: boolean;
   login: (user: User) => void;
   logout: () => void;
 };
@@ -25,33 +26,39 @@ const AuthContext: Context<AuthContextType | undefined> = createContext<
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const storedUser: string | null = localStorage.getItem("user");
     if (storedUser) {
-      setUser(JSON.parse(storedUser));
+      try {
+        setUser(JSON.parse(storedUser));
+      } catch (e) {
+        console.error("Geçersiz user datası:", e);
+        localStorage.removeItem("user");
+      }
     }
+    setLoading(false);
   }, []);
 
-  const login: (user: User) => void = (user: User) => {
-    setUser(user);
-    localStorage.setItem("user", JSON.stringify(user));
+  const login: (userData: User) => void = (userData: User) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
   };
 
   const logout: () => void = () => {
     setUser(null);
-    console.log(localStorage.getItem("user"));
     localStorage.removeItem("user");
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, loading, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-export const useAuth: () => AuthContextType = () => {
+export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
   if (!context) {
     throw new Error("useAuth must be used within an AuthProvider");
